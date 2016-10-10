810a281699b131989363c157378fc9678fa5fc61
beyonddoor <beyonddoor@hotmail.com>
2016-10-10 17:37:35 +0800

    build all and test

---
+++ text_view_bold/build.cmd
@@ -0,0 +1,13 @@
+
+set _ns=com.cirosantilli.android_cheat.text_view_bold
+
+if "%1" == "" ( 
+    set NDK_PROJECT_PATH=%~dp0
+    ndk-build NDK_APPLICATION_MK=.\jni\Application.mk
+    ant debug
+    goto :eof
+)
+
+adb shell pm uninstall %_ns%
+for %%i in (bin\*-debug.apk) do adb install %%i
+adb shell am start -n %_ns%/.Main
