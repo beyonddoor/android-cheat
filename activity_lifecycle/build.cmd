f319c2df36619c4c95faad3d93a8b705ec164973
beyonddoor <beyonddoor@hotmail.com>
2016-10-31 20:17:05 +0800

    test

--- activity_lifecycle/build.cmd
+++ activity_lifecycle/build.cmd
@@ -1,13 +1,14 @@
 
 set _ns=com.cirosantilli.android_cheat.activity_lifecycle
+set mode=release
 
 if "%1" == "" ( 
     set NDK_PROJECT_PATH=%~dp0
     ::ndk-build NDK_APPLICATION_MK=.\jni\Application.mk
-    ant debug
+    ant %mode%
     goto :eof
 )
 
 adb shell pm uninstall %_ns%
-adb install bin\AndroidDebugActivityLifecycle-debug.apk
-adb shell am start -n %_ns%/.Main
+adb install bin\AndroidDebugActivityLifecycle-%mode%.apk
+adb shell am start -D -n %_ns%/.Main
